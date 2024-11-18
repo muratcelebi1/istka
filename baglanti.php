9654c910f4264f52ac6e1fc4c3081d6f8a792ed4
muratcelebi1 <58674725+muratcelebi1@users.noreply.github.com>
2024-11-18 16:54:14 +0300

    düzenleme yapıldı

--- baglanti.php
+++ baglanti.php
@@ -4,27 +4,29 @@ $username = "root";
 $password = "";
 $dbname = "istka";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
+if ($conn->connect_error) {
+  die("Bağlantı hatası: " . $conn->connect_error);
+}
+echo "Database bağlanıldı";
 $sql = "SELECT id, fname, lname FROM user";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+  echo "<br> Görüntülenicek veri mevcut.<br>";
   echo "<table style='border: 1px solid black; border-collapse: collapse;'>";
   echo "<tr><th style='border: 1px solid black; padding: 8px;'>ID</th><th style='border: 1px solid black; padding: 8px;'>Name</th></tr>";
-  // output data of each row
+ 
   while($row = $result->fetch_assoc()) {
     echo "<tr><td style='border: 1px solid black; padding: 8px;'>".$row["id"]."</td><td style='border: 1px solid black; padding: 8px;'>".$row["fname"]." ".$row["lname"]."</td></tr>";
   }
   echo "</table>";
 } else {
-  echo "0 results";
+  echo "0 sonuç";
 }
 $conn->close();
+
 ?>
